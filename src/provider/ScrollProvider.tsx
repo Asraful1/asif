ebb16958f5e8ec6b8d36546b377b8c6f1f4681d4
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-04-20 17:04:12 +0600

    remove lenis for studio

--- src/provider/ScrollProvider.tsx
+++ src/provider/ScrollProvider.tsx
@@ -2,18 +2,27 @@
 
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
+import { usePathname } from "next/navigation";
 
 export function ScrollProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const lenis = new Lenis();
+  const pathname = usePathname();
 
-    function raf(time: number) {
-      lenis.raf(time);
+  useEffect(() => {
+    if (!pathname.includes("/studio")) {
+      const lenis = new Lenis();
+
+      function raf(time: number) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
 
       requestAnimationFrame(raf);
-    }
 
-    requestAnimationFrame(raf);
-  }, []);
+      return () => {
+        lenis.destroy();
+      };
+    }
+  }, [pathname]);
+
   return <>{children}</>;
 }
